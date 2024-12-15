c3ebf9c6b818bed9e6378d2d2509ca950dc36ade
Alpego05 <alba2005pg@gmail.com>
2024-12-15 21:24:43 +0100

    cm2

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
-const trans = document.getElementById("trans");
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const option3 = document.getElementById("option3");
+const option4 = document.getElementById("option4");
 const submit = document.getElementById("submit");
-const result = document.getElementById("result");
+
 
 const ruta = "https://opentdb.com/api.php?amount=1";
 
@@ -16,9 +19,23 @@ const cargarAsy = async () => {
 };
 
 const ShowAnswers = (results) => {
-    results.forEach(item => {
-        if (item.type === "multiple") {
+    results.forEach(result => {
+        if (result.type === "multiple") {
             console.log("respuesta múltiple");
+
+            // Crear un arreglo con todas las respuestas
+            const allAnswers = [...result.incorrect_answers];
+            allAnswers.push(result.correct_answer);
+
+            // Mezclar las respuestas aleatoriamente
+            allAnswers.sort(() => Math.random() - 0.5);
+
+            // Mostrar la pregunta y las opciones en el DOM
+            document.getElementById("result").innerHTML = result.question;
+            option1.innerHTML = allAnswers[0];
+            option2.innerHTML = allAnswers[1];
+            option3.innerHTML = allAnswers[2];
+            option4.innerHTML = allAnswers[3];
         } else {
             console.log("respuesta simple");
         }
@@ -26,7 +43,21 @@ const ShowAnswers = (results) => {
 };
 
 
-document.addEventListener("DOMContentLoaded", cargarAsy);
+
+document.addEventListener("DOMContentLoaded", loadPexelsImage);
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 0:
 // category : "Entertainment: Video Games"
 // correct_answer : "October 31st, 2019"
